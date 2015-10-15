ef6cd6d758f68f60fa86191e18ce769d499296dc
Pierre CLEMENT <pierre@clement.re>
2015-10-15 17:37:23 +0200

    chore(standalone): delete standalone version, another repository has been created

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,7 @@ gulp.task('watch:tests', watchTestsTask);
 gulp.task('watch:typescript', watchTypescriptTask);
 gulp.task('watch', ['watch:typescript', 'watch:test']);
 gulp.task('build:typescript', buildTypescriptTask);
-gulp.task('build:test', buildTestTask);
-gulp.task('build:standalone', buildStandaloneTask);
+gulp.task('build:test', ['typescript'], buildTestTask);
 gulp.task('build:changelog', buildChangelogTask);
 gulp.task('build', buildTask);
 gulp.task('prepublish:checkEverythingCommitted', prepublishCheckEverythingCommittedTask);
@@ -28,12 +27,7 @@ var mocha = require('gulp-mocha');
 var changed = require('gulp-changed');
 var ts = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var uglify = require('gulp-uglify');
 var conventionalChangelog = require('gulp-conventional-changelog');
-var rename = require("gulp-rename");
 var runSequence = require('run-sequence');
 var gutil = require('gulp-util');
 var git = require('gulp-git');
@@ -102,28 +96,6 @@ function buildTestTask() {
   }));
 };
 
-buildStandaloneTask.description = "Build the standalone version of the module";
-function buildStandaloneTask() {
-
-  var b = browserify({
-    entries: './src/data-validation.standalone.js'
-  });
-
-  return b.bundle()
-  .pipe(source('./src/data-validation.standalone.js'))
-  .pipe(buffer())
-  .on('error', gutil.log)
-  .pipe(rename({
-    dirname: "./"
-  }))
-  .pipe(gulp.dest('./dist'))
-  .pipe(uglify())
-  .pipe(rename({
-    suffix: ".min"
-  }))
-  .pipe(gulp.dest('./dist'));
-}
-
 buildChangelogTask.description = "Build the changelog";
 function buildChangelogTask() {
   return gulp.src('CHANGELOG.md', { buffer: false })
@@ -136,36 +108,38 @@ function buildChangelogTask() {
 buildTask.description = "Build the package";
 function buildTask(done) {
   runSequence(
-    'build:typescript', 'build:test', 'build:standalone', 'build:changelog',
+    'build:typescript', 'build:test', 'build:changelog',
     function (error) {
       done(error ? new gutil.PluginError('build', error.message, {showStack: false}) : undefined);
-    })
-  };
+    }
+  );
+};
 
-  /*
-  * Prepublish tasks
-  */
-  prepublishCheckEverythingCommittedTask.description = "Check if everything is committed";
-  function prepublishCheckEverythingCommittedTask(done) {
-    git.status({args: '--porcelain', quiet: true}, function (err, stdout) {
-      var message = err || (stdout.length !== 0 && "Some files are not committed");
-      done(message ? new gutil.PluginError('prepublish:checkEverythingCommitted', message, {showStack: false}) : undefined);
-    });
-  };
+/*
+* Prepublish tasks
+*/
+prepublishCheckEverythingCommittedTask.description = "Check if everything is committed";
+function prepublishCheckEverythingCommittedTask(done) {
+  git.status({args: '--porcelain', quiet: true}, function (err, stdout) {
+    var message = err || (stdout.length !== 0 && "Some files are not committed");
+    done(message ? new gutil.PluginError('prepublish:checkEverythingCommitted', message, {showStack: false}) : undefined);
+  });
+};
 
-  prepublishCheckMasterPushedTask.description = "Check if every commits are pushed on origin";
-  function prepublishCheckMasterPushedTask(done){
-    git.exec({args : 'log origin/master..master', quiet: true}, function (err, stdout) {
-      var message = err || (stdout.length !== 0 && "Commits are not pushed");
-      done(message ? new gutil.PluginError('prepublish:checkMasterPushed', message, {showStack: false}) : undefined);
-    });
-  }
+prepublishCheckMasterPushedTask.description = "Check if every commits are pushed on origin";
+function prepublishCheckMasterPushedTask(done){
+  git.exec({args : 'log origin/master..master', quiet: true}, function (err, stdout) {
+    var message = err || (stdout.length !== 0 && "Commits are not pushed");
+    done(message ? new gutil.PluginError('prepublish:checkMasterPushed', message, {showStack: false}) : undefined);
+  });
+}
 
-  prepublishTask.description = "Run before publish to check if everyhting is fine before the publication";
-  function prepublishTask(done) {
-    runSequence(
-      'build', 'prepublish:checkEverythingCommitted', 'prepublish:checkMasterPushed',
-      function (error) {
-        done(error ? new gutil.PluginError('prepublish', error.message, {showStack: false}) : undefined);
-      })
-    };
+prepublishTask.description = "Run before publish to check if everyhting is fine before the publication";
+function prepublishTask(done) {
+  runSequence(
+    'build', 'prepublish:checkEverythingCommitted', 'prepublish:checkMasterPushed',
+    function (error) {
+      done(error ? new gutil.PluginError('prepublish', error.message, {showStack: false}) : undefined);
+    }
+  );
+};
